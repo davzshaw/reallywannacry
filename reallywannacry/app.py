1401b35fef48ec51a51324375b27c23b2ef969b8
davzshaw <juandis0246@gmail.com>
2024-10-30 17:34:44 -0700

    a

--- reallywannacry/app.py
+++ reallywannacry/app.py
@@ -1,10 +1,10 @@
 from utils import calculateTemperature
-from analyzer import processAudioFiles, isCrying
+#from analyzer import processAudioFiles, isCrying
 if __name__ == "__main__":
   sound = ["c1.wav"]
-  a, b, c, d = processAudioFiles(sound)
-  cry = isCrying(a, b, c, d)
+  #a, b, c, d = processAudioFiles(sound)
+  #cry = isCrying(a, b, c, d)
   temp = calculateTemperature()
-  print("Crying:", cry)
+  #print("Crying:", cry)
   print("Temperature:", temp)
   
